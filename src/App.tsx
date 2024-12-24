bbaae5584844f45899593858485769571dd1c42b
Antoine Capeyron <antoine.cap@hotmail.fr>
2024-12-24 20:11:00 +0100

    emoji wave shake animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './tailwind.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,9 @@ const Home: React.FC = () => {
     <>
       <div className="flex h-screen justify-start items-center text-white text-center">
         <div className="ml-40 text-left">
-          <h1 className="text-5xl mb-4">Hi There ! 👋🏻</h1>
+          <h1 className="text-5xl mb-4 ">
+            Hi There ! <span className="animate-shake">👋🏻</span>
+          </h1>
           <p className="text-5xl flex">
             I'M&nbsp;
             <p className="text-purple-400 font-bold">ANTOINE CAPEYRON</p>
@@ -17,7 +19,7 @@ const Home: React.FC = () => {
           <br />
           <h1 className="text-5xl font-mono text-purple-400 min-h-[3.6rem]">
             <Typewriter
-              words={["Fullstack Developer"]}
+              words={['Fullstack Developer']}
               loop={false}
               cursor
               cursorBlinking={cursorBlinking}
